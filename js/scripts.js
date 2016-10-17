2316eff83f0bd822629f7aa49f670f43d53be776
Levi Bibo and Ryan Peterson <ryan@>
2016-10-17 09:40:54 -0700

    return js code to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -14,20 +14,7 @@ var divide = function(number1, number2) {
 	return (number1 / number2);
 };
 
-var heightConversion = function(heightImperial) {
-	return (parseFloat(heightImperial) / 39.3701);
-};
+var number1 = parseInt(prompt("Enter a number: "));
+var number2 = parseInt(prompt("Enter another number: "));
 
-var weightConversion = function(weightImperial) {
-	return(parseFloat(weightImperial) / 2.2046)
-};
-
-var heightImperial = parseInt(prompt("What is your height in inches: "));
-var weightImperial = parseInt(prompt("What is your weight in pounds: "));
-
-var heightMetric = heightConversion(heightImperial);
-var weightMetric = weightConversion(weightImperial);
-
-var result = (weightMetric/(heightMetric * heightMetric));
-
-alert(result);
+alert(add(number1, number2));
