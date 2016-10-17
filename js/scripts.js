bb99d61cad9144e26409175b19208f2f6a0b6bb8
Levi Bibo and Ryan Peterson <ryan@>
2016-10-17 09:37:10 -0700

    add in bmi calculator

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,32 @@ var add = function(number1, number2) {
 	return (number1 + number2);
 };
 
-var number1 = parseInt(prompt("Enter a number: "));
-var number2 = parseInt(prompt("Enter another number: "));
+var subtract = function(number1, number2) {
+	return (number1 - number2);
+};
 
-alert(add(number1, number2));
+var multiply = function(number1, number2) {
+	return (number1 * number2);
+};
+
+var divide = function(number1, number2) {
+	return (number1 / number2);
+};
+
+var heightConversion = function(heightImperial) {
+	return (parseFloat(heightImperial) / 39.3701);
+};
+
+var weightConversion = function(weightImperial) {
+	return(parseFloat(weightImperial) / 2.2046)
+};
+
+var heightImperial = parseInt(prompt("What is your height in inches: "));
+var weightImperial = parseInt(prompt("What is your weight in pounds: "));
+
+var heightMetric = heightConversion(heightImperial);
+var weightMetric = weightConversion(weightImperial);
+
+var result = (weightMetric/(heightMetric * heightMetric));
+
+alert(result);
