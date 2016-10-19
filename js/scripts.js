366c05661cb2574ee49489fc0602697a657b2209
Jonathan Thom and Ryan Peterson <jonathan@>
2016-10-19 10:08:20 -0700

    Add front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//back end aka business end
+
 var add = function(number1, number2) {
 	return (number1 + number2);
 };
@@ -14,7 +16,15 @@ var divide = function(number1, number2) {
 	return (number1 / number2);
 };
 
-var number1 = parseInt(prompt("Enter a number: "));
-var number2 = parseInt(prompt("Enter another number: "));
 
-alert(add(number1, number2));
+//Front end aka user interface
+
+$(document).ready(function() {
+	$("#add").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+});
